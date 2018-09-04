eef55cda0a0bbc74c03e58966a4ef85b2544a702
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-04 05:38:03 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,9 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 		:railroads => {
-			:pieces => 4
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {}
 		}
 	}
 end
@@ -27,7 +29,10 @@ def monopoly_with_third_tier
 	monopoly = {
 		:railroads => {
 			:pieces => 4,
-			:names => {}
+			:names => {},
+			:rent_in_dollars => {
+				:one_piece_owned => 25
+			}
 		}
 	}
 end
